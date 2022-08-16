a22af89f5cbdfaf5b23c7c051d3754c99811fdb0
dheroju <dherry007@gmail.com>
2022-08-16 22:31:52 +0100

    Styled and added photos to the dictionary app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,30 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="text-center">
-          <small>coded by Aderoju Johnson</small>
+          This project was coded by{" "}
+          <a
+            href="https://Aderoju-Johnson/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Aderoju Johnson
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/dheroju/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://fanciful-monstera-2cece7.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
